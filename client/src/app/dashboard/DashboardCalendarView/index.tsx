ce274bc83e6dd01b8e3d44db27660ea11dea95e1
Gaurav Khadka <gauravkhadka111111@gmail.com>
2025-02-12 12:20:42 +0545

    design like google calendar

--- client/src/app/dashboard/DashboardCalendarView/index.tsx
+++ client/src/app/dashboard/DashboardCalendarView/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import CalendarHeader from '@/components/CalendarHeader'
+import CalendarMainView from '@/components/CalendarMainView'
 
 type Props = {}
 
 const DashboardCalendarView = (props: Props) => {
   return (
-    <div>DashboardCalendarView</div>
+    <div className=''>
+     <CalendarHeader/>
+     <CalendarMainView/>
+      </div>
   )
 }
 
---
+++ client/src/components/CalendarSidebar/create.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+export default function Create() {
+  return (
+    <div>
+        <Button/></div>
+  )
+}
