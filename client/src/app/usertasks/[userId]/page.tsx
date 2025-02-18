464860fd511648b03acd6f50b55a8e8955602afb
Gaurav Khadka <gauravkhadka111111@gmail.com>
2025-02-18 23:48:31 +0545

    created another userTask Header but username is to be fixed

--- client/src/app/usertasks/[userId]/page.tsx
+++ client/src/app/usertasks/[userId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import TaskHeader from "../../TaskHeader";
+import TaskHeader from "../../UserTaskHeader";
 import Board from "../../UserTaskBoardView";
 import ModalNewTask from "@/components/ModalNewTask";
 import { useAuth } from "../../../context/AuthContext"; // Import the custom hook
